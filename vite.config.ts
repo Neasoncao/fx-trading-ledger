2b0f33c73c7c7293d6f3f1d4375e457c300fecae
Neasoncao <xrayriver@126.com>
2026-05-25 12:06:43 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fx-trading-ledger/',  // ← 必须是这个，前后都有斜杠
+  base: '/fx-trading-ledger/',
 })
